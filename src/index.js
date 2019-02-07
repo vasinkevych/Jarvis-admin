cef648ef324d19c1b23634512c1652602e139539
vivanch <vivanch@softserveinc.com>
2019-02-07 13:30:05 +0200

    added editorconfig

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const UsersService = require('../src/services/users.service');
 const Router = require('koa2-router');
 const router = Router();
 router.get('/send-email/:car_number', (ctx) => {
-	UsersService.getUserByNumber(car_number)
-		.then(user => EmailService.sendEmail({
-			to: user.email,
-			subject: 'test subject',
-			html: '<div>Test html...</div>'
-		}))
-		.then((response) => ctx.body = {status: 'ok'})
-		.catch(() => {});
+  UsersService.getUserByNumber(car_number)
+    .then(user => EmailService.sendEmail({
+      to: user.email,
+      subject: 'test subject',
+      html: '<div>Test html...</div>'
+    }))
+    .then((response) => ctx.body = {status: 'ok'})
+    .catch(() => {
+    });
 });
 
 app.use(router);
--- src/services/users.servise.js
+++ src/services/users.servise.js
@@ -1,12 +1,12 @@
 module.exports = {
 
-	// TODO ....
-	getUserByNumber(number) {
-		return Promise.resolve({
-			name: 'test name',
-			email: 'vivanch@softserveinc.com',
-			skype: 'kori_qwe',
-			phoneNumber: '0685435102'
-		});
-	}
+  // TODO ....
+  getUserByNumber(number) {
+    return Promise.resolve({
+      name: 'test name',
+      email: 'vivanch@softserveinc.com',
+      skype: 'kori_qwe',
+      phoneNumber: '0685435102'
+    });
+  }
 };
