f75d9debe8d34ebce9631373fd5dac6185096949
vivanch <vivanch@softserveinc.com>
2019-06-16 17:47:23 +0300

    added test database for node

---
+++ src/unit-tests/create-user-with-car.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const EmailService = require('../services/email.service');
+
+describe.only('emailer', () => {
+  it('should create user and car', done => {
+    assert.equal(true, true);
+    done();
+  });
+});
