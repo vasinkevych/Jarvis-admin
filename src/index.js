17d87358aa0970f379bced8ccb424bbd581a1606
Vitalii Vasinkevych <vasinkevych@gmail.com>
2019-06-24 22:35:03 +0300

    added google api service

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
+require('dotenv').config({ silent: true });
+
 const Koa = require('koa');
 const mount = require('koa-mount');
 const app = new Koa();
 const serve = require('koa-static');
+const path = require('path');
 const createReadStream = require('fs').createReadStream;
 const EmailService = require('./services/email.service');
 const UsersService = require('./services/users.servise');
 const DatabaseService = require('./services/database.service');
+const ParseService = require('./services/parser.service');
+const GoogleSheetToJSON = require('./services/google-api.service');
 
 const graphqlHttp = require('koa-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 
-app.use(serve(__dirname + '/public'));
+app.use(serve(path.join(__dirname, '/public')));
 
 // TODO need to move routes to separated files;
 const Router = require('koa2-router');
 const router = Router();
 
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*');
   await next();
 });
 
 // probably need to use koa-views or something... but if we have only one index.html.....
 router.get('/admin-panel', ctx => {
-  const reader = createReadStream(__dirname + '/public/index.html');
+  const reader = createReadStream(path.join(__dirname, '/public/index.html'));
   return new Promise(resolve => {
     reader.on('data', chunk => {
       ctx.type = 'html';
@@ -45,6 +50,20 @@ router.get('/admin/api/execute-sql', ctx => {
     .catch(err => (ctx.body = err));
 });
 
+router.get('/admin/api/parse', ctx => {
+  const { CLIENT_EMAIL, PRIVATE_KEY, SPREAD_SHEET_ID } = process.env;
+  const gSheetToJSON = new GoogleSheetToJSON({ CLIENT_EMAIL, PRIVATE_KEY });
+
+  return gSheetToJSON
+    .getJson({ spreadsheetId: SPREAD_SHEET_ID, range: 'contacts' })
+    .then(({ data, status = 401, statusText }) => {
+      ctx.status = status;
+      ctx.statusText = statusText;
+      ctx.body = { data: new ParseService().normalizeRows(data.values) };
+    })
+    .catch(err => (ctx.body = err));
+});
+
 /* APIs */
 router.get('/api/send-email/:car_number', ctx => {
   return UsersService.getUserByNumber(ctx.params.car_number)
@@ -52,18 +71,23 @@ router.get('/api/send-email/:car_number', ctx => {
       EmailService.sendEmail({
         to: user.email,
         subject: 'test subject',
-        html: '<div>Test html...</div>',
+        html: '<div>Test html...</div>'
       })
     )
     .then(() => (ctx.body = { status: 'ok' }))
     .catch(() => {});
 });
 
-app.use(mount('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true
-})));
+app.use(
+  mount(
+    '/graphql',
+    graphqlHttp({
+      schema: graphqlSchema,
+      rootValue: graphqlResolver,
+      graphiql: true
+    })
+  )
+);
 
 app.use(router);
 
---
+++ src/services/parser.service.js
@@ -0,0 +1,74 @@
+class ParseService {
+  parsePhoneNumbers(phones) {
+    const COUNTRY_CODE_UA = "380";
+    const PHONE_LENGTH = 9; // length of normalized phone like 971234567
+
+    let phoneCell = phones
+      .trim()
+      .replace(/\D/g, "")
+      .replace(/^380|^\+380|^80|^0+/, "");
+    let index = 0;
+    let normalizedPhone = "";
+    let phonesArray = [];
+    let length = phoneCell.length + 1;
+
+    while (length) {
+      if (normalizedPhone.length < PHONE_LENGTH) {
+        normalizedPhone += phoneCell.charAt(index);
+        index += 1;
+        length -= 1;
+      } else {
+        phonesArray.push(COUNTRY_CODE_UA + normalizedPhone);
+        normalizedPhone = "";
+        phoneCell = phoneCell.substr(index).replace(/^380|^\+380|^80|^0/, "");
+        index = 0;
+        length = phoneCell.length + 1;
+      }
+    }
+
+    return phonesArray;
+  }
+
+  normalizeCarNumber(carNumber) {
+    // TODO create parse function
+    return carNumber;
+  }
+
+  normalizeName(name) {
+    return name.trim();
+  }
+
+  normalizeRows(rawData) {
+    const MAX_CAR_NUMBER_LENGTH = 11;
+
+    return rawData.map((row, i) => {
+      let [user, brand, carNumber, phones, room, internalPhone, skype] = row;
+
+      if (user.length < 1 && i > 0) {
+        // get name from previous row when user cell is empty
+        user = this.normalizeName(rawData[i - 1][0]);
+      } else {
+        user = this.normalizeName(user);
+      }
+
+      carNumber =
+        carNumber > MAX_CAR_NUMBER_LENGTH
+          ? this.normalizeCarNumber(carNumber)
+          : carNumber;
+
+      phones = phones ? this.parsePhoneNumbers(phones) : [];
+
+      return {
+        user,
+        brand,
+        carNumber,
+        phones,
+        room,
+        internalPhone,
+        skype
+      };
+    });
+  }
+}
+
+module.exports = ParseService;
