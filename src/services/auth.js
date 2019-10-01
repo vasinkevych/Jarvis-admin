d074cfe0c0d06659e8c221a89aa71e420871fbe4
vivanch <vivanch@softserveinc.com>
2019-10-01 14:27:57 +0300

    test heroku

--- src/services/auth.js
+++ src/services/auth.js
@@ -18,11 +18,14 @@ const verify = jwt({
 });
 
 const checkJwt = () => async (ctx, next) => {
+  console.log('-------------', 1);
   // if (/^\/api\/(.*)(?:\/|$)/.test(ctx.path) || ctx.path === '/graphql') {
   if (/^\/api\/(.*)(?:\/|$)/.test(ctx.path)) {
+    console.log('-------------', 2);
     await verify.call(this, ctx, next)
       .catch(e => console.log('e=====', e));
   } else {
+    console.log('-------------', 3);
     await next();
   }
 };
