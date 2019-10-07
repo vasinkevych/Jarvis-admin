d2eb3722241612b07b14a41359e5100036a12356
Yurii Chobaniuk <yuriichobaniuk172@gmail.com>
2019-10-07 18:21:48 +0300

    search works

--- client/services/System.js
+++ client/services/System.js
@@ -21,6 +21,7 @@ export async function fetchTableData(query) {
 
 export const filterTable = (tableData, pattern) => {
   if (!pattern) return tableData;
+  if (tableData.length === 0) return tableData;
 
   // in case data includes only numbers then search by phone field (if it exists)
   if (/^\d+$/.test(pattern) && "mobile" in tableData[0]) {
@@ -30,13 +31,42 @@ export const filterTable = (tableData, pattern) => {
     });
   }
 
-  // in case data includes field name then search by name field
-  if ("name" in tableData[0]) {
-    return tableData.filter(dataObj => {
-      let searchValReg = new RegExp(`${pattern}`, "gi");
-      return searchValReg.test(dataObj.name);
-    });
-  }
+  // if filter gains at least one result for the one of the fields it will return it
+  // and skip other fields filtering
+  const filteredData = getFilteredByFields(
+    Object.keys(tableData[0]).filter(objKey => /^[a-z0-9]+$/i.test(objKey)),
+    tableData,
+    pattern
+  );
+
+  if (filteredData.length > 0) return filteredData;
 
   return tableData;
 };
+
+//
+function filterByField(field, array, pattern) {
+  if (array.length === 0) return [];
+  if (Array.isArray(array[0][field])) {
+    return array.filter(dataObj => {
+      const innerArray = dataObj[field].reduce((acc, innerObject) => {
+        return acc.concat(
+          getFilteredByFields(Object.keys(innerObject), dataObj[field], pattern)
+        );
+      }, []);
+      return innerArray.length;
+    });
+  }
+  return array.filter(dataObj => {
+    let searchValReg = new RegExp(`${pattern}`, "gi");
+    return searchValReg.test(dataObj[field]);
+  });
+}
+
+function getFilteredByFields(fieldsList, arrayToFilter, pattern) {
+  return fieldsList.reduce((acc, field) => {
+    if (acc.length === 0)
+      acc = [...filterByField(field, arrayToFilter, pattern)];
+    return acc;
+  }, []);
+}
