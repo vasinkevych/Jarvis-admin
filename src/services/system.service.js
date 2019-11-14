afe1bdfbc5ed677b8a9f267871e407bccba556c2
Yuriy Chobanyuk <sonicmaster172@gmail.com>
2019-11-14 17:23:55 +0200

    basic storage setup

--- src/services/system.service.js
+++ src/services/system.service.js
@@ -1,22 +1,54 @@
 const mysqldump = require('mysqldump');
 const configs = require('../configs');
 const DatabaseService = require('../services/database.service');
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+const admin = require('firebase-admin');
+const fs = require('fs').promises;
 
 module.exports = {
-  getDatabaseDump() {
-    return mysqldump({
-      connection: {
-        host: configs.DATABASE_HOST,
-        database: configs.DATABASE_NAME,
-        user: configs.DATABASE_USER,
-        password: configs.DATABASE_PASSWORD
-      }
-    });
+  async getDatabaseDump() {
+    const filePath = path.resolve(appDir, 'tmp', `${Date.now()}-dump.sql`);
+
+    try {
+      await mysqldump({
+        connection: {
+          host: configs.DATABASE_HOST,
+          database: configs.DATABASE_NAME,
+          user: configs.DATABASE_USER,
+          password: configs.DATABASE_PASSWORD
+        },
+        dumpToFile: filePath
+      });
+    } catch (e) {
+      throw new Error('Dump creation error: ' + e.message);
+    }
+
+    return filePath;
   },
 
-  // TODO
-  saveFileToCloud(sqlDump) {
-    return Promise.resolve();
+  async saveFileToCloud(filePath) {
+    let serviceAccount = configs.FIREBASE_ACCOUNT_CREDITS;
+    let bucket;
+
+    try {
+      await admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket: 'gs://react-native-33df6.appspot.com'
+      });
+
+      bucket = await admin.storage().bucket();
+    } catch (e) {
+      throw new Error('Storage connection error: ' + e.message);
+    }
+
+    try {
+      await bucket.upload(filePath);
+
+      await fs.unlink(filePath);
+    } catch (e) {
+      throw e;
+    }
   },
 
   clearUsersTable() {
