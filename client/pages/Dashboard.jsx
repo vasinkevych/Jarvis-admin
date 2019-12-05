4ae291c7a1562e0fb51be74e2bb0573bed93ac72
Yuriy Chobanyuk <sonicmaster172@gmail.com>
2019-12-05 18:45:52 +0200

    tooltip

--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -6,7 +6,8 @@ import {
   max,
   scaleBand,
   axisLeft,
-  axisBottom
+  axisBottom,
+  event
 } from 'd3';
 import '../styles/Chart.css';
 import { fetchTableData } from '../services/System';
@@ -69,6 +70,11 @@ const Dashboard = () => {
       .call(axisBottom(xScale))
       .attr('transform', `translate(0, ${innerHeight})`);
 
+    const tooltip = select('.svg-container')
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('visibility', 'hidden');
+
     g.selectAll('rect')
       .data(cars)
       .enter()
@@ -76,10 +82,30 @@ const Dashboard = () => {
       .attr('y', d => yScale(yValue(d)))
       .attr('height', yScale.bandwidth())
       .attr('width', d => xScale(xValue(d)))
-      .attr('title', xValue);
+      .attr('title', xValue)
+      .on('mouseover', function() {
+        return tooltip.style('visibility', 'visible');
+      })
+      .on('mousemove', function() {
+        const { offsetX, offsetY } = event;
+        const value = select(this).attr('title');
+        console.log(offsetX, offsetY, value);
+
+        select('.tooltip')
+          .style('top', offsetY - 30 + 'px')
+          .style('left', offsetX - 30 + 'px')
+          .text(value);
+      })
+      .on('mouseout', function() {
+        return tooltip.style('visibility', 'hidden');
+      });
   }, [cars]);
 
-  return <svg className={'car-chart'} width="1000" height="700" />;
+  return (
+    <div className={'svg-container'}>
+      <svg className={'car-chart'} width="1000" height="700" />
+    </div>
+  );
 };
 
 export default Dashboard;
