60214f00536717513186d7e8316ba479ee5adb43
Yuriy Chobanyuk <sonicmaster172@gmail.com>
2019-12-06 16:39:58 +0200

    base functionality

--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -1,104 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import {
-  select,
-  csv,
-  scaleLinear,
-  max,
-  scaleBand,
-  axisLeft,
-  axisBottom,
-  event
-} from 'd3';
+import React, { useContext, useEffect, useState } from 'react';
+import { select } from 'd3';
 import '../styles/Chart.css';
-import { fetchTableData } from '../services/System';
-import { parseCarsModels } from '../services/Chart';
+import { parseCarsModels, generateChart } from '../services/Chart';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import NotificationContext from '../context/alert/notificationContext';
+
+const CARS_QUERY = gql`
+  query getCars {
+    cars {
+      id
+      number
+      brand
+    }
+  }
+`;
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
   const [cars, setCars] = useState([]);
 
-  useEffect(() => {
-    fetchTableData('select * from cars')
-      .then(carsData => carsData.result)
-      .then(carsData => setCars(parseCarsModels(carsData)));
-  }, []);
-
-  console.log(cars);
+  const { error, data } = useQuery(CARS_QUERY);
+  const { notifyError } = useContext(NotificationContext);
 
   useEffect(() => {
-    csv('../utils/data.csv')
-      .then(csvData =>
-        csvData.map(item => ({ ...item, population: +item.population }))
-      )
-      .then(csvData => setData([...csvData]));
-  }, []);
+    if (error) {
+      notifyError('Cars fetch error');
+      return;
+    }
+    if (!data) return;
+    setCars(parseCarsModels(data.cars));
+  }, [error, data]);
 
   useEffect(() => {
     if (!cars.length) return;
     const svg = select('svg');
-    const svgWidth = +svg.attr('width');
-    const svgHeight = +svg.attr('height');
-
-    const xValue = d => d.quantity;
-    const yValue = d => d.brand;
-
-    const margin = {
-      left: 120,
-      top: 20,
-      bottom: 40,
-      right: 20
-    };
-
-    const innerWidth = svgWidth - (margin.left + margin.right);
-    const innerHeight = svgHeight - (margin.bottom + margin.top);
-
-    const xScale = scaleLinear()
-      .domain([0, max(cars, xValue)])
-      .range([0, innerWidth]);
-
-    const yScale = scaleBand()
-      .domain(cars.map(yValue))
-      .range([0, innerHeight])
-      .padding(0.1);
-
-    const g = svg
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-    g.append('g').call(axisLeft(yScale));
-    g.append('g')
-      .call(axisBottom(xScale))
-      .attr('transform', `translate(0, ${innerHeight})`);
-
-    const tooltip = select('.svg-container')
-      .append('div')
-      .attr('class', 'tooltip')
-      .style('visibility', 'hidden');
-
-    g.selectAll('rect')
-      .data(cars)
-      .enter()
-      .append('rect')
-      .attr('y', d => yScale(yValue(d)))
-      .attr('height', yScale.bandwidth())
-      .attr('width', d => xScale(xValue(d)))
-      .attr('title', xValue)
-      .on('mouseover', function() {
-        return tooltip.style('visibility', 'visible');
-      })
-      .on('mousemove', function() {
-        const { offsetX, offsetY } = event;
-        const value = select(this).attr('title');
-        console.log(offsetX, offsetY, value);
-
-        select('.tooltip')
-          .style('top', offsetY - 30 + 'px')
-          .style('left', offsetX - 30 + 'px')
-          .text(value);
-      })
-      .on('mouseout', function() {
-        return tooltip.style('visibility', 'hidden');
-      });
+    generateChart(svg, cars);
   }, [cars]);
 
   return (
--- client/services/Chart.js
+++ client/services/Chart.js
@@ -1,3 +1,14 @@
+import {
+  axisBottom,
+  axisLeft,
+  event,
+  max,
+  scaleBand,
+  scaleLinear,
+  select
+} from 'd3';
+import {selectAll} from "d3-selection";
+
 export const parseCarsModels = carsData => {
   return carsData
     .reduce((acc, car) => {
@@ -13,6 +24,8 @@ export const parseCarsModels = carsData => {
 
       if (brand === 'qashkai') brand = 'nissan';
 
+      if (brand === 'mitsubitshi') brand = 'mitsubishi';
+
       const existingBrand = acc.find(item => item.brand === brand);
       if (existingBrand) {
         existingBrand.quantity += 1;
@@ -25,4 +38,75 @@ export const parseCarsModels = carsData => {
       return acc;
     }, [])
     .sort((a, b) => b.quantity - a.quantity);
+};
+
+export const generateChart = (svg, data) => {
+  const svgWidth = +svg.attr('width');
+  const svgHeight = +svg.attr('height');
+
+  const xValue = d => d.quantity;
+  const yValue = d => d.brand;
+
+  const margin = {
+    left: 120,
+    top: 20,
+    bottom: 40,
+    right: 20
+  };
+
+  const innerWidth = svgWidth - (margin.left + margin.right);
+  const innerHeight = svgHeight - (margin.bottom + margin.top);
+
+  const xScale = scaleLinear()
+    .domain([0, max(data, xValue)])
+    .range([0, innerWidth]);
+
+  const yScale = scaleBand()
+    .domain(data.map(yValue))
+    .range([0, innerHeight])
+    .padding(0.1);
+
+  const g = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  const xAxis = axisBottom(xScale).tickSize(-innerHeight);
+
+  g.append('g').call(axisLeft(yScale));
+  g.append('g')
+    .attr('class', 'chart-body')
+    .call(xAxis)
+    .attr('transform', `translate(0, ${innerHeight})`);
+
+  const container = select('.svg-container');
+  const tooltip = container
+    .append('div')
+    .attr('class', 'tooltip')
+    .style('visibility', 'hidden');
+
+  selectAll('.chart-body .tick text').attr('transform', 'translate(0, 12)');
+
+  g.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('y', d => yScale(yValue(d)))
+    .attr('height', yScale.bandwidth())
+    .attr('width', d => xScale(xValue(d)))
+    .attr('fill', 'steelblue')
+    .attr('title', xValue)
+    .on('mouseover', function() {
+      const el = select(this);
+      el.attr('fill', 'yellow');
+      const value = el.attr('title');
+      tooltip.style('visibility', 'visible').text(value);
+    })
+    .on('mousemove', function() {
+      const { offsetX, offsetY } = event;
+      tooltip.style('top', offsetY + 'px').style('left', offsetX + 'px');
+    })
+    .on('mouseout', function() {
+      select(this).attr('fill', 'steelblue');
+      tooltip.style('visibility', 'hidden');
+    });
 };
